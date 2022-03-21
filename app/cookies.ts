92e120810ef9961c94da7a433d3c0fb45e6fde25
Matt Hova <matthova87@gmail.com>
2022-03-20 17:18:08 -0700

    commit

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,5 +1,5 @@
 import { createCookie } from "remix";
 
 export const userPrefs = createCookie("userPrefs", {
-  maxAge: 604_800, // one week
+  maxAge: 31_536_000, // one year
 });
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,10 +5,46 @@ import {
   ActionFunction,
   Form,
   LoaderFunction,
+  MetaFunction,
 } from "remix";
 
 import { userPrefs } from "~/cookies";
 
+export const meta: MetaFunction = () => {
+  return {
+    description: "TODO",
+    "og:description": "TODO",
+    keywords: [
+      "hovalabs",
+      "hova labs",
+      "the hovas",
+      "design system",
+      "design systems",
+      "css",
+      "css variables",
+      "theme",
+      "light theme",
+      "dark theme",
+      "light mode",
+      "dark mode",
+      "remix",
+      "remix run",
+      "react",
+      "javascript",
+      "typescript",
+    ].join(", "),
+    author: "The Hovas",
+    title: "The perfect dark mode with Remix Run",
+    "og:title": "The perfect dark mode with Remix Run",
+    "og:image": "https://placekitten.com/400/400",
+    "twitter:card": "TODO",
+    "twitter:title": "TODO",
+    "twitter:description": "TODO",
+    "twitter:image:width": "1200",
+    "twitter:image:height": "630",
+    "twitter:image": "https://placekitten.com/400/400",
+  };
+};
 export const action: ActionFunction = async ({ request }) => {
   const cookieHeader = request.headers.get("Cookie");
   const cookie = (await userPrefs.parse(cookieHeader)) || {};
@@ -19,7 +55,7 @@ export const action: ActionFunction = async ({ request }) => {
     cookie.theme = "light";
   } else if (theme === "dark") {
     cookie.theme = "dark";
-  } else if (theme === null) {
+  } else if (theme === "") {
     cookie.theme = null;
   }
 
@@ -42,24 +78,15 @@ export default function Index() {
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>The Perfect Dark Mode</h1>
       <ul>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="https://github.com/HovaLabs/remix-perfect-dark-mode/"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
+            Check out the code on Github
           </a>
         </li>
         <li>
@@ -68,10 +95,24 @@ export default function Index() {
           </a>
         </li>
       </ul>
-      <Form method="post">
-        <input type="hidden" name="theme" value={setThemeTo} />
-        <button type="submit">Toggle Theme</button>
-      </Form>
+      <p>
+        This site is a minimal example of how to set up a Remix app with
+        light/dark mode. The site defaults to the OS's theme preferences
+        (light/dark theme) while also allowing the user to override it,
+        manually. The user's setting is stored in a cookie 🍪. No flash, no
+        fuss, just good clean theming fun.
+      </p>
+      <div style={{ display: "flex" }}>
+        <Form method="post">
+          <input type="hidden" name="theme" value={setThemeTo} />
+          <button type="submit">Toggle Theme</button>
+        </Form>
+        <div style={{ width: 32 }} />
+        <Form method="post">
+          <input type="hidden" name="theme" value={""} />
+          <button type="submit">Reset Theme Cookie</button>
+        </Form>
+      </div>
     </div>
   );
 }
